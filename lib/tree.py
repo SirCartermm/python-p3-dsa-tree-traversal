c92d5cfc4b87f895ee216313913163005ee653e1
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-06 17:02:31 +0300

    modified tree.py

---
+++ lib/tree.py
@@ -0,0 +1,16 @@
+class Tree:
+    def __init__(self, node):
+        self.node = node
+        self.children = []
+
+    def get_element_by_id(self, id):
+        def dfs(node, id):
+            if node['id'] == id:
+                return node
+            for child in node.get('children', []):
+                
+              result = dfs(child, id)
+              if result:
+                  return result
+            return None
+        return dfs(self.node, id)
