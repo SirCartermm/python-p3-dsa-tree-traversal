5d7ed3796c8a1e853d5b08cbe7bb3e5af15fca7d
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-06 17:40:10 +0300

    modified get_element_by_test.py

--- lib/testing/get_element_by_id_test.py
+++ lib/testing/get_element_by_id_test.py
@@ -1,48 +1,6 @@
-from tree import Tree
+import unittest
+from lib.tree import Tree
 
-class TestStack:
-    '''Class Stack in Stack.py'''
-
-    def test_get_element_by_id(self):
-  # <body>
-  #   <div id="main">
-  #     <h1 id="heading">Title</h1>
-  #     <h2>Subtitle</h2>
-  #   </div>
-  # </body>
-
-        '''get_element_by_id test'''
-    tree = Tree(
-      {
-        'tag_name': 'body',
-        'id': None,
-        'children': [
-          {
-            'tag_name': 'div',
-            'id': 'main',
-            'children': [
-              {
-                'tag_name': 'h1',
-                'id': 'heading',
-                'text_content': 'Title',
-                'children': []
-              },
-              {
-                'tag_name': 'h2',
-                'id': None,
-                'text_content': 'Subitle',
-                'children': []
-              }
-            ]
-          }
-        ]
-      }
-    )
-    assert(tree.get_element_by_id('heading') =={
-      'tag_name': 'h1',
-      'id': 'heading',
-      'text_content': 'Title',
-      'children': []
-    } )
-
-    assert(tree.get_element_by_id('nope') == None)
+class TestGetElementId(unittest.TestCase):
+    def test_find_node_by_id(self):
+        
