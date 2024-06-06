8d326807c352a72becaf08616e2747cf80e5e625
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-06 18:07:38 +0300

    modified get_element_by_test.py

--- lib/testing/get_element_by_id_test.py
+++ lib/testing/get_element_by_id_test.py
@@ -3,4 +3,19 @@ from lib.tree import Tree
 
 class TestGetElementId(unittest.TestCase):
     def test_find_node_by_id(self):
+        node = {'id': 'root','children': [
+            {'id: 'child', 'children': [
+                {'id': 'grandchild1', 'children': []},
+                 {'id': 'grandchild2', 'children': []}}
+                  
+             ]},
+             {'id': 'child2, 'children': []}
+              
+            []}
+        tree = Tree(node)
+        self.assertEqual(tree.get_element_by_id('grandchild1')['id'], 'grandchild' )
+        self.assertEqual(tree.get_element_by_id('child'2)['id'], 'child2')
+
+    def test_node_not_found(self):
+        
         
